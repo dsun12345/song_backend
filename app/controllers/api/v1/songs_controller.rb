6fc2763959c089e1a26aa3d380915f9ddd6f35d5
danielsun <danielsun12345@gmail.com>
2020-08-25 20:27:16 -0700

    editing controller

--- app/controllers/api/v1/songs_controller.rb
+++ app/controllers/api/v1/songs_controller.rb
@@ -1,2 +1,23 @@
 class Api::V1::SongsController < ApplicationController
+
+    def index
+        songs = Song.all
+        render json: songs
+    end 
+
+    def create
+        song = Song.new(song_params)
+        if song.save
+            render json: song, status: :accepted
+        else 
+            render json: {errors: song.errors.full_messages}, status: :unprocessible_entity
+        end 
+    end 
+
+private
+
+    def song_params
+        require(:song).permit(:title, :genre, :album_url, :artist_id)
+    end 
+
 end
