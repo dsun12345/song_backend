822983d85edb94a099d48ff89f994e685af40912
danielsun <danielsun12345@gmail.com>
2020-08-26 15:46:14 -0700

    fixing cors issue

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :songs, only: [:index, :create]        
+      resources :songs, only: [:index, :create]    
+      resources :artists, only: [:index]    
     end 
   end 
 
